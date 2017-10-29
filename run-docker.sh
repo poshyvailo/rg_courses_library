1c9989883d13ba687214c62d61f8982a0a3c23cb
poshik <poshik.vova@gmail.com>
2017-10-29 19:43:37 +0200

    The arguments for the docker-run.sh script are added. rmi - removing the container docker, rebuild - re-creating the image

--- run-docker.sh
+++ run-docker.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-# Создаем новый образ, если его еще нет в системе.
-if [[ "$(docker images -q poshik-library:latest 2> /dev/null)" == "" ]]; then
+DOCKER_IMAGE='poshik-library:latest'
+
+# Обновление докер образа
+if [[ $1 == 'rebuild' ]]; then
+    sudo docker rmi $DOCKER_IMAGE
     cp Gemfile docker/Gemfile
-    docker build -t poshik-library:latest docker
+    docker build -t $DOCKER_IMAGE docker
+    rm docker/Gemfile
+    exit 0
+fi
+
+# Удаление докер образа
+if [[ $1 == 'rmi' ]]; then
+    sudo docker rmi $DOCKER_IMAGE
+    exit 0
+fi
+
+# Создаем новый образ, если его еще нет в системе.
+if [[ "$(docker images -q $DOCKER_IMAGE 2> /dev/null)" == "" ]]; then
+    cp Gemfile docker/Gemfile
+    docker build -t $DOCKER_IMAGE docker
     rm docker/Gemfile
 fi
 
+if [[ -n "$1" ]]; then
+    arg=$1
+else
+    arg='run.rb'
+fi
+
 # Запускаем образ на выполнение
-docker run -it --rm -v "$PWD":/usr/src/app poshik-library:latest ruby run.rb
+docker run -it --rm -v "$PWD":/usr/src/app $DOCKER_IMAGE ruby $arg
